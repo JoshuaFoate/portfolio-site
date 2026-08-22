4cb69c7f2f99fba40c151c4d83f13d90319e949e
Joshua Foate <joshuafoate@gmail.com>
2026-08-22 13:00:05 -0500

    Create hero/home section on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main>
-      <section id="home">Home</section>
+      <Hero />
       <section id="about">About</section>
       <section id="experience">Experience</section>
       <section id="projects">Projects</section>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+    return (
+        <section id="home" className="min-h-screen flex flex-col justify-center px-6">
+            <h1 className="text-5xl font-bold">Joshua Foate</h1>
+            <p className="text-xl mt-2">Software Engineer currently living in Autin, TX.</p>
+        </section>
+    );
+}
