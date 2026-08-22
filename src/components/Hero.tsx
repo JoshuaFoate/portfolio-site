2317f543a3541819f53d1788ff5e5b8fcb9ac1a7
Joshua Foate <joshuafoate@gmail.com>
2026-08-22 13:03:57 -0500

    Fix typo from Autin to Austin

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
     return (
         <section id="home" className="min-h-screen flex flex-col justify-center px-6">
             <h1 className="text-5xl font-bold">Joshua Foate</h1>
-            <p className="text-xl mt-2">Software Engineer currently living in Autin, TX.</p>
+            <p className="text-xl mt-2">Software Engineer currently living in Austin, TX.</p>
         </section>
     );
 }
